20c841eb5564199833500ea46ae055027fa55744
DMD82 <rallye.citoyen.82@gmail.com>
2026-05-05 21:02:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 //    à jour la valeur ci-dessous et commitez sur GitHub.
 // ============================================================
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbw_SF2icG5-5-idflzd4lUtk5PO6G5ToIDwfRlZCyOOIyxy0BwT-t4Ovh2ySsQ2969E/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyvkYh2kLrZJGf_BLmRa160nifcjcjRNsklSyc5WGZ6hdk8XgqTm0Hb0w5YnikQ4Nus/exec';
 
 // ============================================================
 // MOTS DE PASSE
