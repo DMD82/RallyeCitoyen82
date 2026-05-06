aea35d46b83939f437103bff15059629376b9141
DMD82 <rallye.citoyen.82@gmail.com>
2026-05-06 09:53:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,8 @@
 
 const API_URL = 'https://script.google.com/macros/s/AKfycbwoQysp2Byf3fCk21MMtmotlBEDBcA5t0F-oesp3OI9PKR7Us37ltkaaCChpPgXYEg7/exec';
 
+const API_NOTATION_URL = 'https://script.google.com/macros/s/AKfycbzrOrLFwOFAHWil_Fn1iHjqbLwvjzv1-9PtyW6lq5mRlrn94HyfkR9AV3iQL4H9p4AKzQ/exec';
+
 // ============================================================
 // MOTS DE PASSE
 // ⚠️ C'est le SEUL endroit à modifier pour changer les MDP
