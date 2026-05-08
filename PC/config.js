986bc62f5a2ddc625b835ba9732f29c6a3af401f
DMD82 <rallye.citoyen.82@gmail.com>
2026-05-09 01:12:33 +0200

    Update config.js

--- PC/config.js
+++ PC/config.js
@@ -9,7 +9,7 @@
 
 const API_URL = 'https://script.google.com/macros/s/AKfycbwrGBjSO8HqwkhH_8fTy7jnwqHCgEvKd59u04pVhMHLk7ld0av5EkKNf2AdXIZlM9Ks/exec';
 
-const API_NOTATION_URL = 'https://script.google.com/macros/s/AKfycbwY6IlNPdlWcIVtI1CVKxE-_wnfcrAT4A2a0gPHrdrQnHASSnup9fIvT80oMx99NbW0Vw/exec';
+const API_NOTATION_URL = 'https://script.google.com/macros/s/AKfycbxHRoBiL5kvv3ZCqiF8-7aciqs2iln_Vd1Pk1d9x335_9OyMR2Az5t21B6o8op_rj0p3w/exec';
 
 // ============================================================
 // MOTS DE PASSE
