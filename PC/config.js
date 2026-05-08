2ede197089682cfdeb538eb7110de6ce3040f7f9
DMD82 <rallye.citoyen.82@gmail.com>
2026-05-09 00:41:37 +0200

    Update API_URL to new endpoint

--- PC/config.js
+++ PC/config.js
@@ -7,7 +7,7 @@
 //    à jour la valeur ci-dessous et commitez sur GitHub.
 // ============================================================
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbzgIfXA02ePm7QasWeap7lu7pThGnjtVcsZEdob6ZGE2fK2ky1oF24EJ1Q97hYqpK99/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwrGBjSO8HqwkhH_8fTy7jnwqHCgEvKd59u04pVhMHLk7ld0av5EkKNf2AdXIZlM9Ks/exec';
 
 const API_NOTATION_URL = 'https://script.google.com/macros/s/AKfycbwLCNFWMXTPUJkrPPTnKebZA81bt4fasMQUbfNLALwCWBvsWvQs_FjfgSP_8NHZOnwRSw/exec';
 
