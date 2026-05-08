5e912428f410da259ab789d0ba17021938f13531
DMD82 <rallye.citoyen.82@gmail.com>
2026-05-09 00:36:44 +0200

    Update config.js

--- PC/config.js
+++ PC/config.js
@@ -7,7 +7,7 @@
 //    à jour la valeur ci-dessous et commitez sur GitHub.
 // ============================================================
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbx-Q22UMkxBZnRlloPimTLuu_bwcEeZUT4ij4tigNlrtWY6rLRgqsh4CBoc6zuPDrH4/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzgIfXA02ePm7QasWeap7lu7pThGnjtVcsZEdob6ZGE2fK2ky1oF24EJ1Q97hYqpK99/exec';
 
 const API_NOTATION_URL = 'https://script.google.com/macros/s/AKfycbwLCNFWMXTPUJkrPPTnKebZA81bt4fasMQUbfNLALwCWBvsWvQs_FjfgSP_8NHZOnwRSw/exec';
 
